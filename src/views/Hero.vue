8bf0301581236a334d9d9e7c1c483d0348e50a3e
Hamilton de Souza <hamilton-souza@outlook.com.br>
2022-02-17 19:37:35 -0300

    new configuration

---
+++ src/views/Hero.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>Essa é a página de heroí</p>
+</template>
+
+<script>
+export default {
+  name: "HeroPage",
+};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>Essa é a página home.</p>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style></style>
