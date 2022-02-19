403c2918892aeef85b85cc9f36cc3546b8c09b24
Hamilton de Souza <hamilton-souza@outlook.com.br>
2022-02-19 20:53:47 -0300

    added id property to hero page component, added data and created method to get hero data based on id

--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -1,10 +1,32 @@
 <template>
-  <p>Essa é a página de heroí</p>
+  <p v-if="hero">{{ hero.name }}</p>
 </template>
 
 <script>
+import axios from "axios";
+import { apiKeyPublic, ts, hash, baseUrl } from "@/utils";
 export default {
   name: "HeroPage",
+  props: ["id"],
+  data() {
+    return {
+      hero: null,
+    };
+  },
+  methods: {
+    getHero() {
+      axios
+        .get(
+          `${baseUrl}/${this.id}?ts=${ts}&apikey=${apiKeyPublic}&hash=${hash}`
+        )
+        .then((response) => {
+          this.hero = response.data.data.results[0];
+        });
+    },
+  },
+  created() {
+    this.getHero();
+  },
 };
 </script>
 
