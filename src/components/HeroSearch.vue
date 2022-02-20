44711a4e9f9a8f418cdd37ac2ba5452a71ce606d
Hamilton de Souza <hamilton-souza@outlook.com.br>
2022-02-19 22:01:59 -0300

    created search hero component, created method to pass in the url the name of the searched hero, created computed url in the HeroesList component, modified the getHero function, added watch in the HeroesList component

---
+++ src/components/HeroSearch.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <input type="text" name="search" id="search" v-model="search" />
+    <input type="submit" value="search" @click.prevent="getHeroName" />
+    {{ search }}
+  </form>
+</template>
+
+<script>
+export default {
+  name: "HeroSearch",
+  data() {
+    return {
+      search: "",
+    };
+  },
+  methods: {
+    getHeroName() {
+      if (this.search) {
+        this.$router.push({ query: { nameStartsWith: this.search } });
+      } else {
+        this.$router.push("");
+      }
+      this.search = "";
+    },
+  },
+};
+</script>
+
+<style></style>
